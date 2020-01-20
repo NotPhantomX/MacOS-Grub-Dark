a8e7f96ab53d22ec0e0fa2698bc6d079963d834a
NotPhantomX <55161664+NotPhantomX@users.noreply.github.com>
2020-01-20 00:47:28 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #! /usr/bin/env bash
 
-THEME='poly-dark'
+THEME='MacOS-Grub-Dark'
 LANG='English'
 
 # Change to temporary directory
@@ -65,7 +65,7 @@ fi
 
 
 echo 'Fetching theme archive'
-wget -O ${THEME}.zip https://github.com/shvchk/${THEME}/archive/master.zip
+wget -O ${THEME}.zip https://github.com/NotPhantomX/${THEME}/archive/master.zip
 
 echo 'Unpacking theme'
 unzip ${THEME}.zip
